4acb2612141eca57f43477aafc73ccdeecec354a
garywanggali <garywang.gali@gmail.com>
2025-09-04 07:42:24 +0800

    增加白车长天线

--- data.js
+++ data.js
@@ -7,4 +7,8 @@ const questions = [
         img :"images/car3_black_long_unclear.png",
         answer: "黑车模糊长天线"
     },
+    {
+        img :"images/car3_white_long.png",
+        answer: "白车长天线"
+    },
 ]
